b4b18a5846c058ddadfb12db6e4b8943a1016ab2
Tim Walsh <timwalsh237@gmail.com>
2020-03-03 15:48:21 -0500

    Can Upload User Image Now

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const functions = require("firebase-functions");
-
 const app = require("express")();
-
 const FBAuth = require("./util/fbAuth");
 
 const { getAllRants, postOneRant } = require("./handlers/rants");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 // Rant Routes
 app.get("/rants", getAllRants);
@@ -14,5 +12,6 @@ app.post("/rant", FBAuth, postOneRant);
 // Users Route
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
