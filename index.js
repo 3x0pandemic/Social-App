52cdc1617ed1c45971c3020d2ee25efde48258e8
Tim Walsh <timwalsh237@gmail.com>
2020-03-04 20:50:34 -0500

    Backend - Notifications can now be posted and for users on Likes, Comments and are  deleted for Unlikes(delete)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
+const { db } = require("./util/admin");
+
 const {
   getAllRants,
   postOneRant,
@@ -37,3 +39,69 @@ app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/rants/${snapshot.data().rantId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            rantId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnLike = functions.firestore
+  .document("likes/{id}")
+  .onDelete(snapshot => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document("comments/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/rants/${snapshot.data().rantId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            rantId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
